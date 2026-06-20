615bd5ca41766c53d1a319f2a3a5812f28163b43
Censera <Censera@proton.me>
2026-06-20 02:38:39 +0100

    added flake.NIX

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "World Cup Polls";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay }:
+    let
+      system = "x86_64-linux";
+      overlays = [ (import rust-overlay) ];
+      pkgs = import nixpkgs { inherit system overlays; };
+      
+      rustToolchain = pkgs.rust-bin.stable.latest.default;
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          rustToolchain
+          pkgs.pkg-config
+          pkgs.openssl
+        ];
+        
+        shellHook = ''
+          clear
+        '';
+      };
+    };
+}
